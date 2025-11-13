952aabf1b783cbe8f4bf9fd65c89b0ed870b6392
Utkarsh bhola <utkarsh.bhola@gmail.com>
2025-11-13 23:40:40 +0530

    removed right panel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import CreatePopup from "./components/CreatePopUp";
 import FriendsPanel from "./components/FriendsPanel";
 import CommunitiesPanel from "./components/Communitiespanel";
 import EventsPanel from "./components/EventsPanel";
-import RightPanel from "./components/RightPanel";
+{/*import RightPanel from "./components/RightPanel";*/}
 
 export default function App() {
   const [activePanel, setActivePanel] = useState(null);
@@ -29,25 +29,31 @@ export default function App() {
     <div className="flex flex-col h-screen overflow-hidden">
       <Navbar />
 
+      {/* 👇 Make this flex row include sidebar, map, and right panel */}
       <div className="flex flex-1">
+        {/* Left sidebar */}
         <Sidebar activePanel={activePanel} setActivePanel={setActivePanel} />
 
+        {/* Slide-in side panel */}
         <SlidePanel
           isOpen={!!activePanel && activePanel !== "create"}
           onClose={() => setActivePanel(null)}
           content={getPanelContent()}
         />
 
+        {/* Center map area */}
         <div className="flex-1 relative">
           <MapView />
         </div>
+
+        {/* 👇 Move the RightPanel inside this main flex row */}
+        {/*<RightPanel />*/}
       </div>
 
+      {/* Popup overlay for create */}
       {activePanel === "create" && (
         <CreatePopup onClose={() => setActivePanel(null)} />
       )}
-
-      <RightPanel />
     </div>
   );
 }
