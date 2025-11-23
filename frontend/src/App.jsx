a62255fb67065255a98236ec4f521c45b6a1a7e2
Utkarsh bhola <utkarsh.bhola@gmail.com>
2025-11-23 19:20:21 +0530

    spell check

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -174,7 +174,7 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/signUp" element={<SignupPage />} />
           <Route
             path="/app"
             element={
