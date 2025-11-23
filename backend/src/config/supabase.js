2a84f3ee830f11f57180815d12631e36c6b1907b
Utkarsh bhola <utkarsh.bhola@gmail.com>
2025-11-23 14:22:13 +0530

    changed ESM

--- backend/src/config/supabase.js
+++ backend/src/config/supabase.js
@@ -1,9 +1,11 @@
-const { createClient } = require("@supabase/supabase-js");
-require("dotenv").config();
+import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_ANON_KEY
 );
 
-module.exports = supabase;
+export default supabase;
